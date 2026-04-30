2f20b1f5387962cf2ec24bce89c7a23ff7a1f2a8
graziella <graziellassilva.8@gmail.com>
2026-04-30 17:29:35 -0300

    css e html + fonts

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ document.addEventListener("DOMContentLoaded", () => {
     menu.classList.toggle("ativo");
   });
 });
+
+// Evita que atualiza a pagina ao clickar em enviar
+const form = document.getElementById("meuForm");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // 🚫 impede o reload da página
+
+  console.log("Form enviado!");
+});
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  form.reset();
+});  
+  //limpa o formulario apos enviar o alerta
